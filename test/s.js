96329a2528ffa692f946dfcfc0f5a1142736978c
James Halliday <mail@substack.net>
2011-08-10 00:49:25 -0700

    passing random sorted test

--- test/s.js
+++ test/s.js
@@ -22,7 +22,15 @@ exports.isSorted = function () {
     assert.ok(!sorted.isSorted([8,7]));
     assert.ok(!sorted.isSorted([3,1,2]));
     assert.ok(!sorted.isSorted([0,1,2,NaN]));
+    assert.ok(!sorted.isSorted([NaN,0,1,2]));
 };
 
 exports.random = function () {
+    for (var i = 0; i < 100; i++) {
+        var xs = sorted();
+        for (var j = 0; j < 10; j++) {
+            xs.push(Math.floor(Math.random() * (j + 1)));
+        }
+        assert.ok(sorted.isSorted(xs.toArray()));
+    }
 };

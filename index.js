41e067660ca4f4865457c5e7d21f584658986a5b
James Halliday <mail@substack.net>
2011-08-09 23:13:54 -0700

    splice() and handle multiple arguments

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ sorted.fromSorted = function (xs, cmp) {
 
 var Sorted = exports.Sorted = function (xs, cmp) {
     this.elements = xs;
+    this.length = xs.length;
     
     this.compare = cmp || function (a, b) {
         return a > b ? 1 : a < b ? -1 : 0;
@@ -29,9 +30,29 @@ var Sorted = exports.Sorted = function (xs, cmp) {
 };
 
 Sorted.prototype.push = Sorted.prototype.unshift = function (x) {
-    var i = this.findIndex(x);
-    this.elements.splice(i, 0, x);
-    return this;
+    if (arguments.length > 1) {
+        for (var i = 0; i < arguments.length; i++) {
+            this.push(arguments[i]);
+        }
+    }
+    else {
+        var i = this.findIndex(x);
+        this.elements.splice(i, 0, x);
+    }
+    
+    this.length = this.elements.length;
+    return this.elements.length;
+};
+
+Sorted.prototype.splice = function (ix, len) {
+    var res = this.elements.splice(ix, len);
+    
+    for (var i = 2; i < arguments.length; i++) {
+        this.push(arguments[i]);
+    }
+    
+    this.length = this.elements.length;
+    return res;
 };
 
 Sorted.prototype.findIndex = function (x) {
@@ -53,4 +74,9 @@ Sorted.prototype.findIndex = function (x) {
     }
     
     return k;
+};
+
+Sorted.prototype.indexOf = function (x) {
+    var i = this.findIndex(x);
+    return this.elements[i] === x ? i : -1;
 };
